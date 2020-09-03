217ab3cfccca5667e1eaeb76d2d96688d140f71d
jogyeongjin <natural0321@naver.com>
2020-09-03 15:23:55 +0900

    d

--- a20_9_3_JDBC/src/a20_9_3_JDBC/JDBCTest1.java
+++ a20_9_3_JDBC/src/a20_9_3_JDBC/JDBCTest1.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 public class JDBCTest1 {
 
 	public static void main(String[] args) throws Exception {
-		String driver="oracle.jdbc.OracleDriver.class";
+		String driver="oracle.jdbc.OracleDriver";
 		String url="jdbc:oracle:thin:@127.0.0.1:1521:xe";
 		String user="system";
 		String password="123456";
